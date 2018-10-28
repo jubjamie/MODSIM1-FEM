13d25d64cfaaedcb15613e358b046b96fffc9143
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-28 15:40:17 +0000

    Add comments to first validtest tests

--- validtest.m
+++ validtest.m
@@ -3,5 +3,7 @@ close all
 p=genpath('lib');addpath(p);p=genpath('status');addpath(p);
 
 problemMesh=OneDimLinearMeshGen(0,1,3);
+%Part 1a
 LaplaceElemMatrix(1,1,problemMesh)
+%Part 1b
 ReactionElemMatrix(1,1,problemMesh)

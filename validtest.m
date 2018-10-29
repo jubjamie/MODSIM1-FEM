49db33089bda2b5bd31116f2c979fb5ac8236821
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-29 20:59:51 +0000

    Temp fix for 1d issue

--- validtest.m
+++ validtest.m
@@ -40,11 +40,11 @@ Problem.c
 %Part 1d
 %Make global matrix and c vector from lecture
 Problem=[];
-Problem.mesh=OneDimLinearMeshGen(0,1,2);
+Problem.mesh=OneDimLinearMeshGen(0,1,10);
 Problem.Diffusion.LE.Generator=@LaplaceElemMatrix;
 Problem.Reaction.LE.Generator=@ReactionElemMatrix;
 Problem.Diffusion.LE.coef=1;
-Problem.Reaction.LE.coef=-9;
+Problem.Reaction.LE.coef=9;
 Problem.f.coef=0;
 Problem.BCS.D=[[0,0];[1,1];];
 [M,c,f,BCrhs,Problem]=globalMatrix(Problem);

9ed534a858cf87b5821b9942b352dca195ae1925
Jamie Cooke <jamie@iamjamiecooke.com>
2018-10-30 17:37:20 +0000

    Write part2a Problem

---
+++ part2a.m
@@ -0,0 +1,22 @@
+function [c,Problem] = part2a(Q,TL)
+%% Answer to Part 2 A
+
+%Init empty problem.
+Problem=[];
+%Define mesh
+Problem.mesh=OneDimLinearMeshGen(0,0.01,10);
+%Define Local element Generator functions
+Problem.Diffusion.LE.Generator=@LaplaceElemMatrix;
+Problem.Reaction.LE.Generator=@ReactionElemMatrix;
+k=1.01e-5;
+%Set coefficients
+Problem.Diffusion.LE.coef=k;
+Problem.Reaction.LE.coef=-Q;
+Problem.f.coef=Q*TL;
+%Set BCs
+Problem.BCS.D=[[323.15,0];[293.15,0.01];];
+[M,~,f,~,Problem]=globalMatrix(Problem);
+Problem.c=M\f;
+c=Problem.c;
+Problem.c
+end

6087d2e94457a80417bc5ea98acb701be897ff75
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-02 19:18:46 +0000

    Write part1a quick script.

---
+++ part1a.m
@@ -0,0 +1,5 @@
+startSolver;
+%The function for Part 1a is LaplaceElemMatrix.m in src/
+%The below function runs the unit test for this function.
+runtests('CourseworkOneUnitTest.m')
+%The success of the above function compeltes this question part.

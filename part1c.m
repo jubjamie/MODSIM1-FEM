ccb0fb2fe35461cc5977985781a3eb85040519e0
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-04 18:50:42 +0000

    Create part1cii comparisons.

--- part1c.m
+++ part1c.m
@@ -1,5 +1,6 @@
 startSolver;
 
+%Part 1ci with two D-BCs
 Problem=[];
 %Define problem.
 Problem.mesh=OneDimLinearMeshGen(0,1,4);
@@ -20,3 +21,28 @@ close all
 
 %Plot the solution with dedicated function.
 plotSolution({Problem},PlotOpts,extraFcn);
+
+%Part 1cii using the example D/N BCs
+Problem2=[];
+%Define problem.
+Problem2.mesh=OneDimLinearMeshGen(0,1,4);
+Problem2.Diffusion.LE.Generator=@LaplaceElemMatrix;
+Problem2.Diffusion.LE.coef=1;
+Problem2.BCS.D=[[0,1];];
+Problem2.BCS.N=[[2,0];];
+
+Problem2=FEMSolve(Problem2);
+
+%Set Plot Options
+PlotOpts=[]; %Clear previous opts.
+PlotOpts.title='Part 1cii Comparison';
+PlotOpts.filepath='status/part1cii_compare.png';
+PlotOpts.x.label='x';
+PlotOpts.y.label='c';
+PlotOpts.legend.labels={'Part 1ci - 2 D BCs','Part 1cii - 1D 1N BCs'};
+PlotOpts.legend.pos='West';
+PlotOpts.nodePlot.Color={'r','g'}';
+extraFcn='grid;';
+
+%Plot the solution with dedicated function.
+plotSolution({Problem, Problem2},PlotOpts,extraFcn);

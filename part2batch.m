fb9e0a53321ae68a3e50113366bcdd757f9d0643
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-04 23:30:30 +0000

    Generate contor vector for part2a.

--- part2batch.m
+++ part2batch.m
@@ -1,7 +1,7 @@
 %%Set up path space
 p=genpath('lib');addpath(p);p=genpath('status');addpath(p);p=genpath('src');addpath(p);
 
-Batch=problemBatch(@part2a,[0.5 294.15 5],[1.5 322.15 5],[3 3 1]);
+Batch=problemBatch(@part2a,[0.5 294.15 5],[1.5 322.15 5],[4 4 1]);
 Batch=solveBatch(Batch);
 
 %Set Plot Options
@@ -15,3 +15,11 @@ close all
 
 %Plot the solution with dedicated function.
 plotSolution(Batch,PlotOpts,extraFcn);
+
+%3D plot max positions.
+f=figure(3);
+[x,y,z]= generateContorMatricies(Batch,1,2,4);
+contourf(x,y,z,10,'ShowText','on');
+xlabel('Q');
+ylabel('TL');
+zlabel('c value');

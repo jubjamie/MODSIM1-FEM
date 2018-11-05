aa33bf00602106f8a4a17db7d8c89ee5e90a1115
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-05 16:25:48 +0000

    Create part2b comparison profiles to part2a.

--- part2bbatch.m
+++ part2bbatch.m
@@ -34,7 +34,7 @@ f11=figure('pos',[400,400,620,475]);
 [x,y,z]= compileResultProfiles(Batch,2,2);
 contourf(x,y,z,50,'ShowText','off','LineColor','none');
 hold on
-[cc,hh]=contour(x,y,z,6,'ShowText','on','LineColor','k');
+[cc,hh]=contour(x,y,z,12,'ShowText','on','LineColor','k');
 hh.LevelList=round(hh.LevelList,0);  %rounds levels to 3rd decimal place
   clabel(cc,hh)
 xlabel('TL');
@@ -55,3 +55,35 @@ ylabel('x');
 zlabel('c value');
 saveas(f12,'status/part2b_profile_Q.png');
 
+%% Compare to part2a.
+Batch2a=problemBatch(@part2a,[0.5 294.15 5],[1.5 322.15 5],[5 5 1]);
+Batch2a=solveBatch(Batch2a);
+
+%Plot difference between a and b.
+f13=figure('pos',[400,400,620,475]);
+[x,y,z]= compileResultProfiles(Batch,2,5);
+[~,~,z2a]= compileResultProfiles(Batch2a,2,5);
+z=z-z2a;
+contourf(x,y,z,50,'ShowText','off','LineColor','none');
+hold on
+[cc,hh]=contour(x,y,z,6,'ShowText','on','LineColor','k');
+hh.LevelStep=1;
+hh.LevelList=round(hh.LevelList,0);  %rounds levels to 3rd decimal place
+  clabel(cc,hh)
+xlabel('TL');
+ylabel('x');
+zlabel('c value');
+saveas(f13,'status/part2a_b_profile_comp.png');
+
+f14=figure(14);
+[x,y,z2b]= generateContorMatricies(Batch,1,2,2);
+[~,~,z2a]= generateContorMatricies(Batch2a,1,2,2);
+z=z2b-z2a;
+[cc,hh]=contourf(x,y,z,10,'ShowText','on');
+hh.LevelStep=0.2;
+hh.LevelList=round(hh.LevelList,2);  %rounds levels to 3rd decimal place
+  clabel(cc,hh)
+xlabel('Q');
+ylabel('TL');
+zlabel('c value');
+saveas(f14,'status/part2a_b_contor_comp.png');

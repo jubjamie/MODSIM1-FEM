e8097bf82ac4936aefadd0d8ba6aaef5358894d4
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-07 17:46:29 +0000

    Adjust part2batch graphs and improve.

--- part2batch.m
+++ part2batch.m
@@ -3,7 +3,7 @@ startSolver;
 
 % Create Batch based on @part2a template interating Q,TL and Number of Elements through
 % [Lower Bounds],[Upper Bounds], [Number of Steps];
-Batch=problemBatch(@part2a,[0.5 294.15 5],[1.5 322.15 5],[5 5 1]);
+Batch=problemBatch(@part2a,[0.5 294.15 10],[1.5 322.15 10],[5 5 1]);
 
 Batch=solveBatch(Batch); % Send to Batch solver using default @FEMSolver.
 
@@ -24,7 +24,7 @@ plotSolution(Batch,PlotOpts,extraFcn);
 %3D plot max positions.
 
 f3=figure('Name','Parameter Space - Part 2a - Constant x Position');
-heldXindex=2;
+heldXindex=4;
 [x,y,z]= generateContorMatricies(Batch,1,2,heldXindex);
 [cc,hh]=contourf(x,y,z,14,'ShowText','on');
 hh.LevelStep=1;
@@ -41,11 +41,11 @@ saveas(f3,'status/part2a_contor.png');
 
 %Plot how temp profile varies with TL
 f4=figure('pos',[400,400,820,475],'Name','Temperature Profile - Part 2a - Varying TL');
-holdPindex=2;
+holdPindex=4;
 [x,y,z]= compileResultProfiles(Batch,2,holdPindex);
 contourf(x,y,z,50,'ShowText','off','LineColor','none');
 hold on
-[cc,hh]=contour(x,y,z,8,'ShowText','on','LineColor','k');
+[cc,hh]=contour(x,y,z,9,'ShowText','on','LineColor','k');
 hh.LevelList=round(hh.LevelList,0);  %rounds levels to 3rd decimal place
   clabel(cc,hh)
 xlabel('TL (K)');
@@ -58,16 +58,19 @@ saveas(f4,'status/part2a_profile.png');
 
 %Plot how temp profile varies with Q
 f5=figure('pos',[400,400,820,475],'Name','Temperature Profile - Part 2a - Varying Q');
-[x,y,z]= compileResultProfiles(Batch,1,2);
+holdPindex=1;
+[x,y,z]= compileResultProfiles(Batch,1,holdPindex);
 contourf(x,y,z,50,'ShowText','off','LineColor','none');
 hold on
-[cc,hh]=contour(x,y,z,12,'ShowText','on','LineColor','k');
+[cc,hh]=contour(x,y,z,9,'ShowText','on','LineColor','k');
 hh.LevelList=round(hh.LevelList,0);  %rounds levels to 3rd decimal place
   clabel(cc,hh)
 xlabel('Q');
 ylabel('x (m)');
 zlabel('c value');
-title('Part 2a Temperature Profile as Q Only Varies');
+holdPterm=2;
+holdPvalue=(((Batch{1}.BatchOptions.UB(holdPterm)-Batch{1}.BatchOptions.LB(holdPterm))/(Batch{1}.BatchOptions.STEPS(holdPterm)-1))*(holdPindex-1))+Batch{1}.BatchOptions.LB(holdPterm);
+title(['Part 2a Temperature Profile as Q Only Varies. TL=' num2str(holdPvalue)]);
 colorbar;
 saveas(f5,'status/part2a_profile_Q.png');
 

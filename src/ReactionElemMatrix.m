93b6540a199aa8b6cba62fc95e3e390e53e9b598
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-27 16:49:30 +0000

    Change reaction to use GQ

--- src/ReactionElemMatrix.m
+++ src/ReactionElemMatrix.m
@@ -12,11 +12,43 @@ function [localElemMatrix] = ReactionElemMatrix(lambda,eID,msh)
 %Convert mesh info to convinienet variable names.
 J=msh.elem(eID).J;
 
+%{
 Int00=(2/3)*lambda*J; % Calculate element values as in report derivation.
 Int01=(1/3)*lambda*J;
 
 localElemMatrix=[Int00, Int01;Int01, Int00]; % Assemble into 2x2 matrix.
+%}
 
+%Int00=INT(lambda*psi0*psi0*J)dz
+%Int01=INT(lambda*psi1*psi0*J)dz
+
+%Use GQ
+N=2;
+gq=makeGQ(N);
+
+% Linear gradients for now.
+
+Int00_gq=zeros(1,N);
+Int01_gq=zeros(1,N);
+
+psi0=@(z) (1-z)/2;
+psi1=@(z) (1+z)/2;
+
+% Int00
+for i=1:N
+    Int00_gq(i)=lambda*psi0(gq.xipts(i))*psi0(gq.xipts(i))*J;
+end
+%No xi(z) to evaluate at gauss point so weights only needed
+Int00=sum(Int00_gq.*gq.gsw);
+
+% Int01
+for i=1:N
+    Int01_gq(i)=lambda*psi1(gq.xipts(i))*psi0(gq.xipts(i))*J;
+end
+%No xi(z) to evaluate at gauss point so weights only needed
+Int01=sum(Int01_gq.*gq.gsw);
+
+localElemMatrix=[Int00, Int01;Int01, Int00];
 
 end
 

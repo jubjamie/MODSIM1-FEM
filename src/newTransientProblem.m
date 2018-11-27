9d3275114fffe06f5a96fb9178f8dbae03af6f84
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-27 21:09:01 +0000

    Add newTransientProblem Class

---
+++ src/newTransientProblem.m
@@ -0,0 +1,39 @@
+classdef newTransientProblem < handle
+    %NEWTRANSIENTPROBLEM Creates a new Transient Problem and methods in a class
+    %   Detailed explanation goes here
+    
+    properties
+       mesh;
+       BCS;
+       Diffusion=struct('Generator',@LaplaceElemMatrix,'coef',0);
+       title;
+       Reaction=struct('Generator',@ReactionElemMatrix,'coef',0);
+       Transient=struct('dt',0.01,'Theta',0.5);
+       f=struct('fcn',@(a,b) 1,'coef',0);
+       GM;
+       c;
+       c_prev;
+       Result;
+       BCrhs;
+       initParams;
+       BatchOptions;
+    end
+    
+    methods
+        function obj = Solve(obj)
+            %NEWPROBLEM Construct an instance of this class
+            %   Detailed explanation goes here
+            obj = FEMSolve(obj);
+        end
+        function obj = DisplayMesh(obj)
+            %NEWPROBLEM Construct an instance of this class
+            %   Detailed explanation goes here
+            DisplayMesh(obj);
+        end
+        function obj = Mesh(obj,s,e,num)
+            obj.mesh = OneDimLinearMeshGen(s,e,num);
+        end
+
+    end
+end
+

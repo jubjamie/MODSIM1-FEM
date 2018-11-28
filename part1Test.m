382b65c1f93910775730995479efcff6def8a25e
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-28 16:21:16 +0000

    Update part1Test.m

--- part1Test.m
+++ part1Test.m
@@ -11,9 +11,17 @@ P.Solve();
 P.PlotAtX(0.8);
 P.PlotAtTime([0.05,0.1,0.3,1]);
 
+x=0;y=0;
 x=linspace(0,10,11);
 for i=1:11
     y(i)=TransientAnalyticSoln(x(i),0.05);
 end
 figure(2)
+plot(x,y)
+
+x=linspace(0,1,101);
+for i=1:101
+    y(i)=TransientAnalyticSoln(0.8,x(i));
+end
+figure(4)
 plot(x,y)

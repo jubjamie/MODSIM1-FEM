bf3150b4a8e618e6246e6fb2eb0c8931e1972fbb
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-28 18:24:41 +0000

    Create part2a quickscript

---
+++ part2a.m
@@ -0,0 +1,18 @@
+startSolver;
+
+close all;
+
+P2=newTransientProblem();
+P2.Mesh(0,0.01,100);
+
+%Set coefficients based on Skin
+Skin=Skin();
+
+P2.Diffusion.coef=[[0,Skin.E.TC];[Skin.E.xend,Skin.D.TC];[Skin.D.xend,Skin.B.TC]];
+
+P2.Transient.Time=50;
+P2.Transient.Theta=1;
+P2.ConstantInit(310.15);
+P2.BCS.D=[[373.15,0];[310.15,0.01];];
+P2.Solve();
+changeTime=P2.PlotAtTime([2,5,10]);

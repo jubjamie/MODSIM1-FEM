6a3f3dd71de8924ab1e80b15c93f04df050be95d
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-29 20:32:54 +0000

    Make part2a speedier and complete

--- part2a.m
+++ part2a.m
@@ -12,7 +12,9 @@ P2.Diffusion.coef=[[0,Skin.E.TC];[Skin.E.xend,Skin.D.TC];[Skin.D.xend,Skin.B.TC]
 
 P2.Transient.Time=50;
 P2.Transient.Theta=1;
+P2.Transient.dt=0.1;
 P2.ConstantInit(310.15);
-P2.BCS.D=[[373.15,0];[310.15,0.01];];
+P2.BCS.D=[[393.15,0];[310.15,0.01];];
 P2.Solve();
 changeTime=P2.PlotAtTime([2,5,10]);
+calculateBurn(P2)

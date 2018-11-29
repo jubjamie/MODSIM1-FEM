18b0c762a5f2adb5ec18161a8b86666a0d1dd3c4
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-29 21:02:55 +0000

    Change about part2b quick scripts
    Make part2b use parfor to find the value.

--- part2b.m
+++ part2b.m
@@ -1,22 +1,25 @@
 startSolver;
 
 close all;
+Skin=Skin();
+temps=[328:0.5:329];
+gammas=zeros(1,size(temps,2));
 
+parfor i=1:size(temps,2)
 P2=newTransientProblem();
 P2.Mesh(0,0.01,100);
 
 %Set coefficients based on Skin
-Skin=Skin();
+
 
 P2.Diffusion.coef=[[0,Skin.E.TC];[Skin.E.xend,Skin.D.TC];[Skin.D.xend,Skin.B.TC]];
+disp(temps(i));
 
 P2.Transient.Time=50;
 P2.Transient.Theta=1;
 P2.Transient.dt=0.1;
 P2.ConstantInit(310.15);
-opts = optimset('fminsearch');
-opts.Display = 'iter'; %What to display in command window
-opts.TolX = 0.5; %Tolerance on the variation in the parameters
-opts.TolFun = 1e15; %Tolerance on the error
-opts.MaxIter = 30; %Max number of iterations
-[x, diff, exitflag] = fminsearchbnd(@(x)burnCostFunction(x,P2), [353.15], [315.15], [393.15], opts);
+P2.BCS.D=[[temps(i),0];[310.15,0.01];];
+P2.Solve(false);
+gammas(i)=calculateBurn(P2);
+end

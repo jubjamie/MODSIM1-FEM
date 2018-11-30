ea767a5cc62b9a8e533d1a989156b7e068ac961f
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-30 11:10:33 +0000

    Add time cut off to profile maker

--- generateTransientProfile.m
+++ generateTransientProfile.m
@@ -1,8 +1,13 @@
-function [x,y,z] = generateTransientProfile(Problem)
+function [x,y,z] = generateTransientProfile(Problem,timeRange)
 %GENERATETRANSIENTPROFILE Summary of this function goes here
 %   Detailed explanation goes here
+if timeRange=='all'
 y=Problem.mesh.nvec;
 x=linspace(0,Problem.Transient.Time,(Problem.Transient.Time/Problem.Transient.dt)+1);
 z=Problem.Solution;
+else
+y=Problem.mesh.nvec;
+x=linspace(0,timeRange,(timeRange/Problem.Transient.dt)+1);
+z=Problem.Solution(:,1:(timeRange/Problem.Transient.dt)+1);    
 end
 

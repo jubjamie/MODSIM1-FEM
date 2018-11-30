83b4142022229529fd49f810c1bbdc5616631f4e
Jamie Cooke <jamie@iamjamiecooke.com>
2018-11-30 20:16:29 +0000

    Create plotSkin.m

---
+++ src/plotTools/plotSkin.m
@@ -0,0 +1,4 @@
+vline([Skin.E.xend,Skin.D.xend],'k--');
+text(Skin.E.xend/2,396,'\leftarrow Epidermis \rightarrow','HorizontalAlignment', 'center');
+text((Skin.D.xend+Skin.E.xend)/2,396,'\leftarrow Dermis  \rightarrow','HorizontalAlignment', 'center');
+text(((Skin.B.xend+Skin.D.xend)/2)-0.0005,396,'\leftarrow Sub-Cutaneous  \rightarrow','HorizontalAlignment', 'center');

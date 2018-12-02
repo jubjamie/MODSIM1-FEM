35efb605d425e266a18dcaf69ca56f69ff56c670
Jamie Cooke <jamie@iamjamiecooke.com>
2018-12-02 16:26:06 +0000

    Make transient source vector accept linear methods

--- src/variableStaticSourceVector.m
+++ src/variableStaticSourceVector.m
@@ -35,21 +35,31 @@ Int0_gq=zeros(1,N);
 Int1_gq=zeros(1,N);
 Int2_gq=zeros(1,N);
 
+if strcmp(msh.basisType,'Quad')
 psi0=@(z) (z.*(z-1))./2;
 psi1=@(z) 1-z.^2;
 psi2=@(z) (z.*(1+z))./2;
+else
+psi0=@(z) (1-z)./2;
+psi1=@(z) (1+z)./2;
+end
 
 % Int00
 Int0_gq=sourcecoef.*psi0(gq.xipts).*J;
 Int1_gq=sourcecoef.*psi1(gq.xipts).*J;
+if strcmp(msh.basisType,'Quad')
 Int2_gq=sourcecoef.*psi2(gq.xipts).*J;
+end
 
 %No xi(z) to evaluate at gauss point so weights only needed
 Int0=sum(Int0_gq.*gq.gsw);
 Int1=sum(Int1_gq.*gq.gsw);
 Int2=sum(Int2_gq.*gq.gsw);
 
-
+if strcmp(msh.basisType,'Quad')
 sourceLocalElem=[Int0,Int1,Int2]';
+else
+    sourceLocalElem=[Int0,Int1]';
+end
 end
 

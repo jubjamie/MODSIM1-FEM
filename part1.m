7ad35f0625caaf8dbb2d3d3b0df806b9b230b138
Jamie Cooke <jamie@iamjamiecooke.com>
2018-12-03 12:33:37 +0000

    Part 1 graph formattting and flex.

--- part1.m
+++ part1.m
@@ -1,6 +1,7 @@
 startSolver;
 close all
-clear x y
+clear x y xE yE
+x_point=0.8; % Used for error analysis and x plotting.
 % Creat a new transient Problem
 P=newTransientProblem();
 P.Mesh(0,1,10);
@@ -10,17 +11,17 @@ P.Transient.Theta=0.5;
 P.BCS.D=[[0,0];[1,1];];
 P.basisType='Linear';
 P.Solve();
-fixedX=P.PlotAtX(0.8);
+fixedX=P.PlotAtX(x_point);
 changeTime=P.PlotAtTime([0.05,0.1,0.3,1]);
 timeStepCount=(P.Transient.Time/P.Transient.dt)+1;
 figure(fixedX);
-title('x=0.8 Varying Over Time - Crank Nicolson');
+title(['x=' num2str(x_point) ' Varying Over Time - Crank Nicolson']);
 hold on;
 x=linspace(0,1,timeStepCount);
 for i=1:timeStepCount
-    y(i)=TransientAnalyticSoln(0.8,x(i));
+    y(i)=TransientAnalyticSoln(x_point,x(i));
 end
-plot(x,y,'DisplayName','Analytical Solution - x: 0.8')
+plot(x,y,'DisplayName',['Analytical Solution - x:' num2str(x_point)])
 saveas(fixedX,['status/cw2/part1_theta_' num2str(P.Transient.Theta) '_' P.basisType '.png']);
 
 
@@ -37,17 +38,17 @@ PE.Transient.Theta=1;
 PE.BCS.D=[[0,0];[1,1];];
 PE.basisType='Linear';
 PE.Solve();
-fixedXE=PE.PlotAtX(0.8);
+fixedXE=PE.PlotAtX(x_point);
 changeTimeE=PE.PlotAtTime([0.05,0.1,0.3,1]);
 timeStepCountE=(PE.Transient.Time/PE.Transient.dt)+1;
 figure(fixedXE);
-title('x=0.8 Varying Over Time - Backwards Euler');
+title(['x=' num2str(x_point) ' Varying Over Time - Backwards Euler']);
 hold on;
 xE=linspace(0,1,timeStepCountE);
 for i=1:timeStepCountE
-    yE(i)=TransientAnalyticSoln(0.8,xE(i));
+    yE(i)=TransientAnalyticSoln(x_point,xE(i));
 end
-plot(xE,yE,'DisplayName','Analytical Solution - x: 0.8');
+plot(xE,yE,'DisplayName',['Analytical Solution - x: ' num2str(x_point)]);
 saveas(fixedXE,['status/cw2/part1_theta_' num2str(PE.Transient.Theta) '_' PE.basisType '.png']);
 
 
@@ -58,8 +59,11 @@ saveas(changeTimeE,['status/cw2/part1_time_overview_theta_' num2str(PE.Transient
 
 errorCompare=figure();
 hline(0,'k--');
-hold on
-plot(x,(y-P.GetValuesAtX(0.8)),'r-','DisplayName','Crank-Nicolson Error');
-plot(xE,(yE-PE.GetValuesAtX(0.8)),'b-','DisplayName','Backward Euler Error');
+hold on;
+plot(x,(y-P.GetValuesAtX(x_point)),'r-','DisplayName','Crank-Nicolson Error');
+plot(xE,(yE-PE.GetValuesAtX(x_point)),'b-','DisplayName','Backward Euler Error');
+xlabel('Time (s)');
+ylabel('Error (Ce-Cx)');
 legend('Location','Northeast');
-
+title(['Error from Analytical Solution at x=' num2str(x_point)]);
+saveas(errorCompare,['status/cw2/part1_error_x-' num2str(x_point) '_' PE.basisType '.png']);

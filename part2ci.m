7c9d2ce33d4df447fa1353df164dba7a43ce7da4
Jamie Cooke <jamie@iamjamiecooke.com>
2018-12-04 15:15:01 +0000

    Small updates

--- part2ci.m
+++ part2ci.m
@@ -14,7 +14,7 @@ P2C.f.coef=[[0,Skin.E.HS];[Skin.E.xend,Skin.D.HS*Skin.D.Tb];[Skin.D.xend,Skin.B.
 
 P2C.Transient.Time=50;
 P2C.Transient.Theta=1;
-P2C.Transient.dt=0.05;
+P2C.Transient.dt=0.005;
 P2C.BCS.D=[[393.15,0];[310.15,0.01];];
 P2C.ConstantInit(310.15);
 P2C.basisType='Quad';
